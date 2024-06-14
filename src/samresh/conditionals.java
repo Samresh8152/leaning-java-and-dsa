d7d7d8de7087b34161d0436855291e492b4b2fdb
Samresh8152 <samreshc31@gmal.com>
2024-06-15 03:05:14 +0530

    first commit

---
+++ src/samresh/conditionals.java
@@ -0,0 +1,22 @@
+package samresh;
+
+public class conditionals {
+    public static void main(String[] args) {
+        /*
+        if(boolean expression T OR F){
+        body
+        }
+            else{
+        body
+        }
+        */
+
+        int age = 20;
+        if(age>=18){
+            System.out.println("can vote");
+        }
+        else{
+            System.out.println("cannnot vote");
+        }
+    }
+}
